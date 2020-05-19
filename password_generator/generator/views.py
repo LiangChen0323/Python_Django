46d8d36554104f5e96dbe3dc40bf265d771e7a9b
liangchen0323 <chenliang323@hotmail.com>
2020-05-19 13:33:29 +0100

    finished password generator project

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -10,9 +10,21 @@ def home(request):
 def password(request):
     password = ""
     characters = list("abcdefghijklmnopqrstuvwxyz")
-    length = 10
-    thepassword = ""
+    if request.GET.get("uppercase"):
+        characters.extend(list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+
+    if request.GET.get("Specialcharacters"):
+        characters.extend(list("!@#$%^&*()"))
+
+    if request.GET.get("numbers"):
+        characters.extend(list("1234567890"))
+
+    length = int(request.GET.get("length"), 12)
     for x in range(length):
         password += random.choice(characters)
 
     return render(request, "generator/password.html", {"password": password})
+
+
+def about(request):
+    return render(request, "generator/about.html")
